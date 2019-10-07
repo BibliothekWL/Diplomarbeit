fef3032d867383fc761f688794f86dd01b9c1f03
KevChela <kevin.chelappurath@htl.rennweg.at>
2019-10-08 00:03:54 +0200

    Installation of Vue except VueRouter and Vuex

---
+++ public/frontend/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <span>
+        <home-hero></home-hero>
+        <home-details></home-details>
+    </span>
+</template>
+
+<script>
+
+    export default {
+        name: 'home'
+    };
+</script>
