ed522a058cf3dd2b96dfffb8335b73230b253503
Zijlstra Niklas <niklas.zijlstra@htl.rennweg.at>
2019-10-24 09:53:11 +0200

    web_routes fix

--- routes/web.php
+++ routes/web.php
@@ -61,9 +61,6 @@ Route::get('/books/mybooks/json',function(){
     return BooksResource::collection(User::findOrFail(auth()->user()->id)->books);
 });
 
-Route::post('/login/json',function(){
-    return json
-});
 
 
 
