6f80b879f077eb86b330711e2c340100c19a9ea4
CorKalom <koziol.k1@hotmail.com>
2019-10-24 09:55:48 +0200

    Commit to Merge

--- public/frontend/src/views/Login.vue
+++ public/frontend/src/views/Login.vue
@@ -1,24 +1,20 @@
 <template>
     <v-container fluid>
         <v-layout>
-<<<<<<< HEAD
             <div class="col-5 lg">
-                <v-text-field label="Login">Hello</v-text-field>
-                <v-text-field label="Passwort">Hello</v-text-field>
+                <v-text-field label="Login" v-model="id" required>Hello</v-text-field>
+                <v-text-field label="Passwort" v-model="password" required>Hello</v-text-field>
             </div>
             <div>
-                <v-btn absolute="true" right="true">Login</v-btn>
+                <v-btn absolute right v-on:click="login">Login</v-btn>
             </div>
-=======
-            <input v-model="id" placeholder="ID">
-            <input v-model="password" placeholder="password">
-            <v-btn v-on:click="login">Login</v-btn>
->>>>>>> 75514d4ad58bd7f4844d304681ca7ffe04b3cb26
         </v-layout>
     </v-container>
 </template>
 
 <script>
+    import axios from "axios";
+
     export default {
         name: "Login",
         data() {
@@ -26,6 +22,19 @@
                 id: "",
                 password: ""
             }
+        },
+        mounted(){
+            axios.get('/api/login', {
+                params: {
+                    id: this.id,
+                    pw: this.password
+                }
+            })
+                .then(response =>{
+                console.log(response.data)
+            }).catch(error =>{
+                console.log(error.message)
+            })
         },
         methods: {
             login: function () {
