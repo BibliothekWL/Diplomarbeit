f0fcc9f165956d80e82bb9bf30d09018dc4088c9
Zijlstra Niklas <niklas.zijlstra@htl.rennweg.at>
2019-11-05 11:53:03 +0100

    Merge branch 'niklasBE' of D:\5Klasse(5AI)\Diplomarbeit with conflicts.

---
+++ resources/js/routes.js
@@ -0,0 +1,32 @@
+import Vue from 'vue';
+import VueRouter from "vue-router";
+
+import BookList from "@/js/components/BookList";
+import Home from "@/js/components/Home";
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+   mode: 'history',
+   routes: [
+       {
+           path: '/',
+           name: 'home',
+           component: Home
+       },
+       {
+           path: '/list',
+           name: 'list',
+           component: BookList
+       },
+       {
+           dsd:hasDocumentSupportd,
+           dd:sdf,
+           component: Login
+       },
+   ]
+});
+
+
+
+export default router;
