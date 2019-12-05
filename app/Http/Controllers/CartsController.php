8e4bf7f8af073c8b1e4fa38c42ba27ce4f93e3e7
Felix Linhart <linhartfelix0@gmail.com>
2019-12-05 13:50:01 +0100

    fixed create cart in backend

--- app/Http/Controllers/CartsController.php
+++ app/Http/Controllers/CartsController.php
@@ -9,21 +9,22 @@ class CartsController extends Controller
 {
     public function create()
     {
-        if(Book::all()->where('id',request()->bookId)->first()->borrowed == 1){
-            dd('this book is not available');
+        $json = file_get_contents('php://input');
+        $jsonarray = json_decode($json, true);
+        if(Book::all()->where('id',$jsonarray['id'])->first()->borrowed == 1){
+            return json_encode(['status' => 400, 'statusMessage' => 'Buch ist schon ausgeborgt']);
         }
-        elseif(Cart::all()->where('book_id',request()->bookId)->isEmpty()==false) {
-            dd('this book is reserved for a user');
+        elseif(Cart::all()->where('book_id',$jsonarray['id'])->isEmpty()==false) {
+            return json_encode(['status' => 400, 'statusMessage' => 'Buch ist schon reserviert']);
         }
         else{
             $cart = new Cart;
             $cart->user_id = auth()->user()->id;
-            $cart->book_id = request()->bookId;
+            $cart->book_id = $jsonarray['id'];
             $cart->save();
-
             $this->index();
+            return json_encode(['status' => 200, 'statusMessage' => 'cart was created successfully']);
         }
-        return(redirect('/cart'));
     }
 
     public function deleteAllCartsFromUser(){
